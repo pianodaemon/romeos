940901f54eb19ff889e29c54e292b015ba799398
Edwin Pĺauchu <pianodaemon@gmail.com>
2022-04-30 18:50:40 -0500

    half-baked skeleton of formats
    Signed-off-by: Edwin Pĺauchu <pianodaemon@gmail.com>

---
+++ DOS/e-invoice/service/formats/src/main/java/com/immortalcrab/formats/FacturaPdf.java
@@ -0,0 +1,23 @@
+package com.immortalcrab.formats;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import com.immortalcrab.opaque.engine.CfdiRequest;
+import com.immortalcrab.opaque.engine.Storage;
+import com.immortalcrab.opaque.error.FormatError;
+import com.immortalcrab.opaque.error.StorageError;
+import com.immortalcrab.qrcode.QRCode;
+
+public class FacturaPdf {
+
+}
---
+++ DOS/e-invoice/service/formats/src/main/java/com/immortalcrab/formats/FacturaXml.java
@@ -0,0 +1,9 @@
+package com.immortalcrab.formats;
+
+import com.immortalcrab.fantastic4.CMoneda;
+import com.immortalcrab.fantastic4.CPais;
+import com.immortalcrab.fantastic4.CTipoDeComprobante;
+
+public class FacturaXml {
+
+}
