7b4d145bf19f511e7b5766115f1e967efff721d2
Edwin Pĺauchu <pianodaemon@gmail.com>
2022-04-30 18:28:58 -0500

    We satisfied dependencies of the issue controller
    Signed-off-by: Edwin Pĺauchu <pianodaemon@gmail.com>

--- DOS/e-invoice/service/server/src/main/java/com/immortalcrab/flow/pipeline/IssueController.java
+++ DOS/e-invoice/service/server/src/main/java/com/immortalcrab/flow/pipeline/IssueController.java
@@ -1,5 +1,10 @@
 package com.immortalcrab.flow.pipeline;
 
+import com.immortalcrab.opaque.error.CfdiRequestError;
+import com.immortalcrab.opaque.error.DecodeError;
+import com.immortalcrab.opaque.error.FormatError;
+import com.immortalcrab.opaque.error.PipelineError;
+import com.immortalcrab.opaque.error.StorageError;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -20,4 +25,39 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class IssueController {
 
+    @RequestMapping(
+            path = "/{kind}",
+            method = RequestMethod.POST,
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    ResponseEntity<Map<String, Object>> issue(
+            @PathVariable("kind") String kind,
+            @RequestPart MultipartFile tokensDoc) throws IOException {
+
+        Map<String, Object> rhm = new HashMap<>() {
+            {
+                put("code", 0);
+                put("desc", "");
+            }
+        };
+
+        InputStream is = tokensDoc.getInputStream();
+
+        try {
+            String uuid = Pipeline.issue(kind, new InputStreamReader(is));
+            rhm.put("desc", uuid);
+
+        } catch (FormatError | DecodeError | CfdiRequestError | PipelineError | StorageError ex) {
+
+            LOGGER.error(ex.getMessage());
+
+            rhm.put("code", ex.getErrorCode());
+            rhm.put("desc", ex.getMessage());
+
+            return new ResponseEntity<>(rhm, HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(rhm, HttpStatus.CREATED);
+    }
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 }
